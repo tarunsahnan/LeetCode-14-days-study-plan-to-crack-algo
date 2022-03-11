7beaa81634742ce99561eeb17b4f3280c369d7c3
tarunsahnan <tarunsahnan98@gmail.com>
2022-03-11 15:34:21 +0530

    day 13

---
+++ Day 13 Bit Manipulation/2 number of 1 bits.cpp
@@ -0,0 +1,16 @@
+//Question Link --> https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+     
+        int count=0;
+        
+        while(n>0){
+             
+            count++;
+            n=n&(n-1);
+        }
+        return count;   
+    }
+};
